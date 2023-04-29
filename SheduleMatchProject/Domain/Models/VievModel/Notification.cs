80f8edf8a17b1b7595a03f3070f955b72c48747b
CjLuka <l.swinicki@onet.pl>
2023-04-29 17:55:02 +0200

    Added walidation for login page

---
+++ SheduleMatchProject/Domain/Models/VievModel/Notification.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Models.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models.VievModel
+{
+    
+    public class Notification
+    {
+        public string Message { get; set; }
+        public NotificationType Type { get; set; }
+    }
+}
--- SheduleMatchProject/SheduleMatchWeb/Pages/Account/Login.cshtml.cs
+++ SheduleMatchProject/SheduleMatchWeb/Pages/Account/Login.cshtml.cs
@@ -1,6 +1,10 @@
 using Domain.Models.Domain;
+using Domain.Models.VievModel;
+using Domain.Response;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.IdentityModel.Tokens;
+using System.Text.Json;
 
 namespace SheduleMatchWeb.Pages.Account
 {
@@ -10,6 +14,25 @@ namespace SheduleMatchWeb.Pages.Account
         public User User { get; set; }
         public void OnGet()
         {
+
+        }
+        public async Task <IActionResult> OnPostAsync()
+        {
+            if (User.Email.IsNullOrEmpty() || User.Password.IsNullOrEmpty())
+            {
+                var notification = new Notification
+                {
+                    Type = Domain.Models.Enum.NotificationType.Error,
+                    Message = "Uzupe³nij wszystkie wymagane pola!"
+
+                };
+                TempData["Notification"] = JsonSerializer.Serialize(notification);
+                ViewData["MessageValidation"] = "Uzupe³nij wszystkie wymagane pola!";
+                return Page();
+                
+                
+            }
+            return RedirectToPage("/Index");
         }
     }
 }
