88936454eb7af0150d62a20fcd57a4bfc0a0a94d
CjLuka <l.swinicki@onet.pl>
2023-05-01 11:46:43 +0200

    Created new database Auth

--- SheduleMatchProject/SheduleMatchWeb/Program.cs
+++ SheduleMatchProject/SheduleMatchWeb/Program.cs
@@ -1,5 +1,6 @@
 using Aplication.Services.Interfaces;
 using Aplication.Services.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Persistance.Data;
 using Persistance.Repo.Interfaces;
@@ -9,7 +10,14 @@ using System.Reflection;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")));
+        builder.Configuration.GetConnectionString("DefaultConnection")));//po³¹czenie z jedn¹ baz¹
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("AuthDbConnectionString")));//po³¹czenie z drug¹ baz¹
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>()
+    .AddEntityFrameworkStores<AuthDbContext>();
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<IClubRepository, ClubRepository>();
@@ -17,6 +25,7 @@ builder.Services.AddScoped<IGameClassRepository, GameClassRepository>();
 builder.Services.AddScoped<IClubServices, ClubServices>();
 builder.Services.AddScoped<IGameClassServices, GameClassServices>();
 builder.Services.AddAutoMapper(Assembly.GetExecutingAssembly());
+builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -32,7 +41,10 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
+
 app.UseAuthorization();
+
 
 app.MapRazorPages();
 
